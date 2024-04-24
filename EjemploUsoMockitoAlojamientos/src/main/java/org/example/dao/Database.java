020276c093baf42d238391d1baf80f915d8f48a1
Gema Garcia <gema.garciafernandez@educa.madrid.org>
2024-04-24 12:42:01 +0200

    junit DAO

--- EjemploUsoMockitoAlojamientos/src/main/java/org/example/dao/Database.java
+++ EjemploUsoMockitoAlojamientos/src/main/java/org/example/dao/Database.java
@@ -13,7 +13,7 @@ public class Database {
 
     public Database() {
         this.alojamientos = new ArrayList<>();
-        //String nombre, String provincia, double precio, ArrayList<Integer> valoraciones,boolean piscina
+        /*//String nombre, String provincia, double precio, ArrayList<Integer> valoraciones,boolean piscina
         alojamientos.add(new Hotel("Melia", "Madrid", 45, new ArrayList<>(), 5));
         alojamientos.add(new Hotel("NH", "Murcia", 30, new ArrayList<>(), 3));
         alojamientos.add(new Hotel("Hilton", "Madrid", 63, new ArrayList<>(), 4));
@@ -26,7 +26,7 @@ public class Database {
             alojamiento.getValoraciones().add(r.nextInt(6));
             alojamiento.getValoraciones().add(r.nextInt(6));
             alojamiento.getValoraciones().add(r.nextInt(6));
-        }
+        }*/
     }
 
     public Database (List<Alojamiento> alojamientos){
